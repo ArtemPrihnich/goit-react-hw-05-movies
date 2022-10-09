9f4f645dab3a6b01868a6859ef2aa989446ed860
ArtemPrihnich <artem.prihnich@gmail.com>
2022-10-09 18:27:36 +0300

    убраны console.log

--- src/components/CastList/CastList.jsx
+++ src/components/CastList/CastList.jsx
@@ -5,7 +5,6 @@ import { List } from './CastList.styled'
 export default function CastList({ data }) {
     return (
         <List>
-            {console.log(data)}
             {data.map((item) => {
                 return (
                     <CastListItem key={item.cast_id} data={item} />
